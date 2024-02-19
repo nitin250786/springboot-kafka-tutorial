85f86852c25f942ed485d1178ae124498333c499
nitin25786 <nitinsharmaji25@gmail.com>
2024-02-19 21:31:20 +0530

    first commit

---
+++ src/main/java/net/javaguides/springboot/kafka/JsonKafkaConsumer.java
@@ -0,0 +1,20 @@
+package net.javaguides.springboot.kafka;
+
+import net.javaguides.springboot.payload.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaConsumer {
+
+    private static  final Logger LOGGER = LoggerFactory.getLogger("JsonKafkaConsumer");
+
+    @KafkaListener(topics = "${spring.kafka.topic-json.name}",groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(User user){
+        LOGGER.info(String.format("Message message received -> %s",user.toString()));
+
+    }
+
+}
